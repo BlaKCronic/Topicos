8aeba61ea7140007ed7dcff6c4c74614d8ecd5c3
CHRISTIAN <poncegonzalez849@gmail.com>
2024-08-29 21:28:15 -0600

    Continuacion calculadora

--- src/main/java/org/example/topicos/HelloApplication.java
+++ src/main/java/org/example/topicos/HelloApplication.java
@@ -4,31 +4,39 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import org.example.topicos.Vistas.Calculadora;
+
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-
-    private Button btn1, btn2, btn3;
-    private VBox vbox;
-    private HBox hbox;
+    private BorderPane bdpPrincipal;
+    private MenuBar menBar;
+    private Menu menCompetencia1, menuCompetencia2, menSalir;
+    private MenuItem mitCalc;
 
     public void CrearUI(){
-        btn1 = new Button("boton 1");
-        btn2 = new Button("Boton 2");
-        btn3 = new Button("Boton 3");
-        vbox = new VBox(btn1, btn2, btn3);
-        hbox = new HBox(btn1, btn2, btn3);
+        mitCalc = new MenuItem("Calculadora");
+        mitCalc.setOnAction(actionEvent -> new Calculadora());
+        menCompetencia1 = new Menu("Competencia 1");
+        menCompetencia1.getItems().addAll(mitCalc);
+        menBar = new MenuBar(menCompetencia1);
+        bdpPrincipal = new BorderPane();
+        bdpPrincipal.setTop(menBar);
     }
 
     @Override
     public void start(Stage stage) throws IOException {
         //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         CrearUI();
-        Scene scene = new Scene(hbox, 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(bdpPrincipal, 320, 240);
+        stage.setTitle("sabes que");
         stage.setScene(scene);
         stage.setMaximized(true);
         stage.show();
---
+++ src/main/java/org/example/topicos/Vistas/Calculadora.java
@@ -0,0 +1,55 @@
+package org.example.topicos.Vistas;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Calculadora extends Stage {
+
+    private Button[][] arrbtn;
+    private TextField txtPantalla;
+    private GridPane teclado;
+    private VBox vbox;
+    private Scene escena;
+    private String[] strTeclas = {"7","8","9","*","4","5","6","/","1","2","3","+","0",".","=","-"};
+
+    private void CrearUI(){
+        arrbtn = new Button[4][4];
+        txtPantalla = new TextField("");
+        txtPantalla.setAlignment(Pos.CENTER_RIGHT);
+        txtPantalla.setEditable(false);
+        teclado = new GridPane();
+        CrearTeclado();
+        vbox = new VBox(txtPantalla, teclado);
+        escena = new Scene(vbox, 200, 200);
+    }
+
+    private void CrearTeclado(){
+        for (int i = 0; i < arrbtn.length; i++){
+            for (int j = 0; j < arrbtn.length; j++) {
+                arrbtn[j][i] = new Button(strTeclas[4*i+j]);
+                arrbtn[j][i].setPrefSize(50, 50);
+                int finalI = i;
+                int finalJ = j;
+                arrbtn[j][i].setOnAction(actionEvent -> detectarTecla(strTeclas[4* finalI + finalJ]));
+                teclado.add(arrbtn[j][i],j,i);
+            }
+        }
+    }
+
+    public Calculadora(){
+        CrearUI();
+        this.setTitle("Calculadora");
+        this.setScene(escena);
+        this.show();
+    }
+
+    private void detectarTecla(String tecla){
+        txtPantalla.appendText(tecla);
+    }
+
+}
