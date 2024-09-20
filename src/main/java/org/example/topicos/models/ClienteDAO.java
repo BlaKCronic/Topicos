05f4f1e59099c348415d80f68ffe1973ba772d6f
CHRISTIAN <poncegonzalez849@gmail.com>
2024-09-20 08:50:37 -0600

    databese

---
+++ src/main/java/org/example/topicos/models/ClienteDAO.java
@@ -0,0 +1,40 @@
+package org.example.topicos.models;
+
+public class ClienteDAO {
+    private int idCliente;
+    private String nombreCte;
+    private String telCte;
+    private String emailCte;
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getNombreCte() {
+        return nombreCte;
+    }
+
+    public void setNombreCte(String nombreCte) {
+        this.nombreCte = nombreCte;
+    }
+
+    public String getTelCte() {
+        return telCte;
+    }
+
+    public void setTelCte(String telCte) {
+        this.telCte = telCte;
+    }
+
+    public String getEmailCte() {
+        return emailCte;
+    }
+
+    public void setEmailCte(String emailCte) {
+        this.emailCte = emailCte;
+    }
+}
